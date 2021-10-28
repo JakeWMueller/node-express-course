005b53cecb2fe99d475def7480471709c27779c7
JakeWMueller <jake.william.mueller@gmail.com>
2021-10-27 23:08:20 -0500

    dynamic GET route added. Typo taken out

--- server.js
+++ server.js
@@ -7,13 +7,22 @@ const mockUserData = [
 ]
 app.get('/users', function(req, res){
     res.json({
-        succes: true,
+        success: true,
         message: 'successfully got users. Yay.',
         users: mockUserData
     })
 })
 
 PORT = 8000;
+
+app.get('/users/:id', function(req, res) {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
+})
 
 app.listen(8000, function(){
     console.log("Server is running on port " + PORT)
