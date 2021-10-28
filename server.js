7b424c942936fbdcdcfd476b25bd27051131efb4
JakeWMueller <jake.william.mueller@gmail.com>
2021-10-28 00:47:35 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const mockUserData = [
     {name: 'Mark'},
@@ -13,8 +14,7 @@ app.get('/users', function(req, res){
     })
 })
 
-PORT = 8000;
-
+// colons are used as variables that can be viewed in the params
 app.get('/users/:id', function(req, res) {
     console.log(req.params.id)
     res.json({
@@ -23,6 +23,32 @@ app.get('/users/:id', function(req, res) {
         user: req.params.id
     })
 })
+
+app.post('/login', function (req, res) {
+    // Typically passwords are encrypted using bcrypt before sending to the database
+    const username=req.body.username;
+    const password=req.body.password;
+
+    // This should come from the database
+    const mockUsername = "billyBob";
+    const mockPassword = "superSecret";
+
+    if (username===mockUsername && password === mockPassword){
+        // in practice, use JSON web token sign method here to make an encrypted token
+        res.json({
+            success: true,
+            message: 'password and username match',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
+
+PORT = 8000;
 
 app.listen(8000, function(){
     console.log("Server is running on port " + PORT)
